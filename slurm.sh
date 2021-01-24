a41903445fbff9bd76338cdc146359ced26112dd
charan n.a <charan1994@gmail.com>
2021-01-24 17:39:24 -0500

    changes to slurm script

--- slurm.sh
+++ slurm.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-#SBATCH --mem=32000 --constraint=CPU-E5-2660
+#SBATCH --mem=64000
 #SBATCH --account=jzola
-#SBATCH --partition=general-compute
-#SBATCH --qos=general-compute
+#SBATCH --partition=planex
+#SBATCH --qos=planex
 #SBATCH --exclusive
+#SBATCH --clusters=faculty
 #SBATCH --job-name="Rust"
 #SBATCH --output=%j.stdout
 #SBATCH --error=%j.stderr
@@ -15,5 +16,14 @@
 date
 module load intel-mpi/2020.1
 # singularity is already part of runtime so need to import new modules
-echo "Running with 1 node"
-srun --mpi=pmi2 singularity run --bind $I_MPI_ROOT/intel64:/opt/intel/oneapi/mpi/2021.1.1 run-test.simg
+echo "Running with 1 node and 8 ranks"
+# size of message is message_size * 4 
+# 4b 32b 64b 128b 256b 512b 1kb 2kb 4kb 8kb 16kb 32kb 64kb 128kb 256kb 512kb 1mb 2mb 4mb 8mb 16mb 32mb 64mb 128mb
+message_sizes=(1 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216 33554432)
+
+for message_size in ${message_sizes[@]}; do
+    for turn in {1..5}; do
+        echo "running code for turn: $turn and message size: $message_size"
+        srun --mpi=pmi2 singularity run --bind $I_MPI_ROOT/intel64:/opt/intel/oneapi/mpi/2021.1.1 run-test.simg $message_size
+    done
+done
